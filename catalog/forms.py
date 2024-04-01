83a104ea7095465e6cd210f6843e1751a6d09bf9
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-01 13:41:10 +0300

    add search and other forms in forms.py

---
+++ catalog/forms.py
@@ -0,0 +1,91 @@
+from django import forms
+from catalog.models import Cook, DishType, Dish
+
+
+class DishForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class CookCreationForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = "__all__"
+
+
+class CookUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data.get("years_of_experience")
+        if years_of_experience < 0:
+            raise forms.ValidationError(
+                "Years of experience cannot be negative"
+            )
+        return years_of_experience
+
+
+class DishTypeCreationForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = "__all__"
+
+
+class DishTypeUpdateForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = "__all__"
+
+
+class DishCreationForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class DishUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username"
+            }
+        )
+    )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
