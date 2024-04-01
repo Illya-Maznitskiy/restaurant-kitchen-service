f9dfdf277e130dd2928cd208d40038380e7d46bf
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-01 10:24:59 +0300

    implemented models for Cook, DishType and Dish

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cooks)
