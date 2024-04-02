2c5a7f07bd42272be9996c01b902815c16f6868f
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-02 16:24:36 +0300

    add correct logic for urls.py and sidebar.html

---
+++ restaurant_kitchen/urls.py
@@ -0,0 +1,76 @@
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from restaurant_kitchen.views import (
+    index,
+    CookListView,
+    CookDetailView,
+    CookCreateView,
+    CookUpdateView,
+    CookDeleteView,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+    DishListView,
+    DishDetailView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+    toggle_assign_to_dish,
+)
+
+
+app_name = "restaurant_kitchen"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("admin/", admin.site.urls),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update"
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete"
+    ),
+    path("dishtypes/", DishTypeListView.as_view(), name="dishtype-list"),
+    path(
+        "dishtypes/create/",
+        DishTypeCreateView.as_view(),
+        name="dishtype-create"
+    ),
+    path(
+        "dishtypes/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dishtype-update"
+    ),
+    path(
+        "dishtypes/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dishtype-delete"
+    ),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
+    path(
+        "dishes/<int:pk>/toggle-assign-cook/",
+        toggle_assign_to_dish,
+        name="toggle-assign-cook-to-dish"
+    ),
+]
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -14,81 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from restaurant_kitchen.views import (
-    index,
-    CookListView,
-    CookDetailView,
-    CookCreateView,
-    CookUpdateView,
-    CookDeleteView,
-    DishTypeListView,
-    DishTypeCreateView,
-    DishTypeUpdateView,
-    DishTypeDeleteView,
-    DishListView,
-    DishDetailView,
-    DishCreateView,
-    DishUpdateView,
-    DishDeleteView,
-    toggle_assign_to_dish,
-)
 
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("admin/", admin.site.urls),
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("cooks/", CookListView.as_view(), name="cook-list"),
-    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
-    path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
-    path(
-        "cooks/<int:pk>/update/",
-        CookUpdateView.as_view(),
-        name="cook-update"
-    ),
-    path(
-        "cooks/<int:pk>/delete/",
-        CookDeleteView.as_view(),
-        name="cook-delete"
-    ),
-    path("dishtypes/", DishTypeListView.as_view(), name="dishtype-list"),
-    path(
-        "dishtypes/create/",
-        DishTypeCreateView.as_view(),
-        name="dishtype-create"
-    ),
-    path(
-        "dishtypes/<int:pk>/update/",
-        DishTypeUpdateView.as_view(),
-        name="dishtype-update"
-    ),
-    path(
-        "dishtypes/<int:pk>/delete/",
-        DishTypeDeleteView.as_view(),
-        name="dishtype-delete"
-    ),
-    path("dishes/", DishListView.as_view(), name="dish-list"),
-    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
-    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
-    path(
-        "dishes/<int:pk>/update/",
-        DishUpdateView.as_view(),
-        name="dish-update"
-    ),
-    path(
-        "dishes/<int:pk>/delete/",
-        DishDeleteView.as_view(),
-        name="dish-delete"
-    ),
-    path(
-        "dishes/<int:pk>/toggle-assign-cook/",
-        toggle_assign_to_dish,
-        name="toggle-assign-cook-to-dish"
-    ),
+    path('admin/', admin.site.urls),
+    path('', include('restaurant_kitchen.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
-
-app_name = "kitchen"
