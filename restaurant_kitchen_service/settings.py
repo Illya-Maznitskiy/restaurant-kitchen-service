1185a6ec396fb273665637d1702fd43239bc1b58
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-02 13:49:07 +0300

    modify settings.py and login.html to create a successful login page

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant_kitchen',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'restaurant_kitchen.Cook'
 
