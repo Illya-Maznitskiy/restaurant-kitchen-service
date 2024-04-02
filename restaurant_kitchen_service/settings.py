d4246c33669b4e4bf1a5a5eda0442992b2c97d25
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-02 12:55:43 +0300

    fix problem with migrations; add migrations to repository

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant_kitchen',
 ]
+
+AUTH_USER_MODEL = 'restaurant_kitchen.Cook'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
