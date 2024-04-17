44d3b791f779f48f8e5c2f8cd58cf5563acffbd4
Illya Maznitskiy <comercaleuros@gmail.com>
2024-04-17 20:32:08 +0300

    update readme with screenshots and update forms for selecting cooks

--- restaurant_kitchen/forms.py
+++ restaurant_kitchen/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from restaurant_kitchen.models import Cook, DishType, Dish
@@ -8,6 +9,43 @@ class DishForm(forms.ModelForm):
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+class DishCreationForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+class DishUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
 
 
 class CookCreationForm(forms.ModelForm):
@@ -40,30 +78,6 @@ class CookUpdateForm(forms.ModelForm):
         return years_of_experience
 
 
-class DishTypeCreationForm(forms.ModelForm):
-    class Meta:
-        model = DishType
-        fields = "__all__"
-
-
-class DishTypeUpdateForm(forms.ModelForm):
-    class Meta:
-        model = DishType
-        fields = "__all__"
-
-
-class DishCreationForm(forms.ModelForm):
-    class Meta:
-        model = Dish
-        fields = "__all__"
-
-
-class DishUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Dish
-        fields = "__all__"
-
-
 class CookSearchForm(forms.Form):
     username = forms.CharField(
         max_length=255,
@@ -77,20 +91,19 @@ class CookSearchForm(forms.Form):
     )
 
 
+class DishTypeCreationForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = "__all__"
+
+
+class DishTypeUpdateForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = "__all__"
+
+
 class DishTypeSearchForm(forms.Form):
-    name = forms.CharField(
-        max_length=255,
-        required=False,
-        label="",
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "Search by name"
-            }
-        )
-    )
-
-
-class DishSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
         required=False,
